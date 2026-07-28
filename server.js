805d4a67b0e43f5f5e23c4b344fa25c3470620ad
eduardo-saurus <jrebano2002@gmail.com>
2026-07-28 16:57:48 +0800

    Add files via upload
    ACTIVITY2

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
-
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/status', (req, res) => {
+  res.json({ status: "active", message: "Operational" });
+});
+
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
+
